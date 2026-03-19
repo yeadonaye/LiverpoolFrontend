de62c5cee4db8991cd82baf6859cba3cb5191336
Nathan Tekeste <nathantek2006@gmail.com>
2026-03-19 11:24:55 +0100

    j ai mis authapi dans une fonction

--- Controleur/GestionLogin.php
+++ Controleur/GestionLogin.php
@@ -1,8 +1,14 @@
 <?php
 // auth.php lives in Modele/DAO
 require_once __DIR__ . '/../Modele/DAO/authapi.php';
+/*
+if (is_jwt_valid($jwt, $secret)) {
+    header('Location: /index.php');
+    exit;
+}
+*/
 
-
+seConnecter();
 
 
 ?>
