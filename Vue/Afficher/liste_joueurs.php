3056c2d37134f961429f14751c79cc01990b4c0c
SENTAYEHU Yeadonaye Ashenafi <yeadonaye-ashenafi.sentayehu@etu.iut-tlse3.fr>
2026-03-29 22:14:52 +0200

    Ajout  d'un fichier pour gerer la suppression et test du lien entre le frontend et le backend

--- Vue/Afficher/liste_joueurs.php
+++ Vue/Afficher/liste_joueurs.php
@@ -1,4 +1,19 @@
-<?php include '../../Controleur/afficher/afficher_joueur.php'; ?>
+<?php
+session_start();
+require_once '../../routeClient.php';
+
+// Redirection si non connecté
+if (!isset($_SESSION['token'])) {
+    header('Location: ../../login.php');
+    exit;
+}
+
+$token = $_SESSION['token'];
+
+$response = routeClient::getJoueurs($token);
+$joueurs  = $response['data'] ?? [];
+$error    = ($response['status_code'] !== 200) ? ($response['status_message'] ?? 'Erreur inconnue') : '';
+?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
---
+++ Vue/Supprimer/supprimer_joueur.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+require_once '../../routeClient.php';
+
+if (!isset($_SESSION['token'])) {
+    header('Location: ../../login.php');
+    exit;
+}
+
+$id = (int)($_GET['id'] ?? 0);
+if ($id) {
+    routeClient::deleteJoueur($id, $_SESSION['token']);
+}
+
+header('Location: liste_joueurs.php');
+exit;
+?>
